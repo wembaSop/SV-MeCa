d8d0cac129d2408c48773292852102863cd471db
Rudel Fankep <rfankep@yahoo.com>
2024-04-19 13:32:56 +0200

    add wrapper and fix _no_chr entry type

--- run_svmeca_root.sh
+++ run_svmeca_root.sh
@@ -1,17 +1,30 @@
-#!/usr/bin/bash 
-set -Eeuo pipefail
+#!/bin/bash
 
-LOG_D=".log"
-CONDA=""
+# Script version
+SCRIPT_VERSION="1.0"
+
+LOG_D="/tmp/.log"
+
+FMT=""
 SAMPLE=""
-BAM=""
-WORK="scratch/${SAMPLE}"
-BED=""
-REF=""
-LOG="${LOG_D}/.nextflow_${SAMPLE}.log"
-SONIC=""
-RUN_OPT=""
+BUILD=""
+NO_CHR=""
 
+INPUT=""
+REF=""
+BED=""
+
+BD=""
+DL=""
+IS=""
+LP=""
+MT=""
+PD=""
+TD=""
+STS=""
+EXTRA=""
+
+PARAMS=""
 
 export NXF_DISABLE_CHECK_LATEST=true
 
@@ -20,30 +33,188 @@ if [ ! -d "$LOG_D" ]; then
     mkdir -p "$LOG_D"
 fi
 
-echo """
-################ RUNNING COMMAND WITH PARAMETERS #############
-nextflow -bg -log $LOG run main.nf \
-        -w $WORK \
-        --myConda $CONDA \
-        --bed $BED \
-        --input $BAM \
-        --sample $SAMPLE \
-        --sonic $SONIC \
-        --containerBind "$WORK" \
-        --reference $REF $RUN_OPT&>>"$LOG_D/${SAMPLE}_out_err.log"
-################    END    ###################################
+usage() {
+    echo "Usage: $0 <BAM|VCF> [options]"
+    echo "Version: $SCRIPT_VERSION"
+    echo "Description: This script is used to run the SV-MeCa pipeline,"
+    echo "             meta-caller for structural variant detection," 
+    echo "             depending on the user input format. BAM or VCF files."
+    echo "Options:"
+    echo "  For BAM:"
+    echo "    -bam <bam>: Input BAM file"
+    echo "    -ref <fasta|fa>: Reference genome file"
+    echo "    -bed <bed>: BED file (optional)"
+    echo "  For VCF:"
+    echo "    -bd <vcf>: BreakDancer VCF"
+    echo "    -dl <vcf>: Delly VCF"
+    echo "    -is <vcf>: INSurVeyor VCF"
+    echo "    -lp <vcf>: Lumpy VCF"
+    echo "    -mt <vcf>: Manta VCF"
+    echo "    -pd <vcf>: Pindel VCF"
+    echo "    -td <vcf>: TARDIS VCF"
+    echo "    -st <txt>: statistic txt file"
+    echo "General options:"
+    echo "    -sample <value>: Sample name (required)"
+    echo "    -build <hg38|hg19>: Build value (required)"
+    echo "    -no_chr <true|false>: Specify whether to include chr prefix (required)"
+    echo "    -extra <extra_value>: Extra nextflow options (optional)"
+    exit 1
+}
 
-""" >"$LOG_D/${SAMPLE}_out_err.log"
+if [ "$#" -lt 1 ]; then
+    usage
+fi
 
-nextflow -bg -log $LOG run main.nf \
-        -w $WORK \
-        --myConda $CONDA \
-        --bed $BED \
-        --input $BAM \
-        --sample $SAMPLE \
-        --sonic $SONIC \
-        --containerBind "$WORK" \
-        --reference $REF $RUN_OPT&>>"$LOG_D/${SAMPLE}_out_err.log"
+FMT=$1
+shift
 
+case $FMT in
+    "bam")
+        while [ "$#" -gt 0 ]; do
+            case $1 in
+                -bam)
+                    INPUT=$2
+                    shift 2
+                    ;;
+                -ref)
+                    REF=$2
+                    shift 2
+                    ;;
+                -bed)
+                    BED=$2
+                    shift 2
+                    ;;
+                -sample)
+                    SAMPLE=$2
+                    shift 2
+                    ;;
+                -build)
+                    case $2 in
+                        "hg38"|"hg19")
+                            BUILD=$2
+                            shift 2
+                            ;;
+                        *)
+                            echo "Invalid value for -build. Accepted values are 'hg38' or 'hg19'"
+                            usage
+                            ;;
+                    esac
+                    ;;
+                -no_chr)
+                    case $2 in
+                        "true"|"false")
+                            NO_CHR=$2
+                            shift 2
+                            ;;
+                        *)
+                            echo "Invalid value for -no_chr. Accepted values are 'true' or 'false'"
+                            usage
+                            ;;
+                    esac
+                    ;;
+                -extra)
+                    EXTRA=" $2"
+                    shift 2
+                    ;;
+                *)
+                    usage
+                    ;;
+            esac
+        done
+        if [ -z "$INPUT" ] || [ -z "$REF" ] || [ -z "$SAMPLE" ] || [ -z "$BUILD" ] || [ -z "$NO_CHR" ]; then
+            echo "Missing required parameters for BAM format"
+            usage
+        fi
+        PARAMS="--input $INPUT --reference $REF --bed $BED" #"--build $BUILD --sample $SAMPLE --no_chr $NO_CHR"
+        ;;
+    "vcf")
+        while [ "$#" -gt 0 ]; do
+            case $1 in
+                -bd)
+                    BD=$2
+                    shift 2
+                    ;;
+                -dl)
+                    DL=$2
+                    shift 2
+                    ;;
+                -is)
+                    IS=$2
+                    shift 2
+                    ;;
+                -lp)
+                    LP=$2
+                    shift 2
+                    ;;
+                -mt)
+                    MT=$2
+                    shift 2
+                    ;;
+                -pd)
+                    PD=$2
+                    shift 2
+                    ;;
+                -td)
+                    TD=$2
+                    shift 2
+                    ;;
+                -st)
+                    STS=$2
+                    shift 2
+                    ;;
+                -sample)
+                    SAMPLE=$2
+                    shift 2
+                    ;;
+                -build)
+                    case $2 in
+                        "hg38"|"hg19")
+                            BUILD=$2
+                            shift 2
+                            ;;
+                        *)
+                            echo "Invalid value for -build. Accepted values are 'hg38' or 'hg19'"
+                            usage
+                            ;;
+                    esac
+                    ;;
+                -no_chr)
+                    case $2 in
+                        "true"|"false")
+                            NO_CHR=$2
+                            shift 2
+                            ;;
+                        *)
+                            echo "Invalid value for -no_chr. Accepted values are 'true' or 'false'"
+                            usage
+                            ;;
+                    esac
+                    ;;
+                -extra)
+                    EXTRA=" $2"
+                    shift 2
+                    ;;
+                *)
+                    usage
+                    ;;
+            esac
+        done
+        if [ -z "$BD" ] || [ -z "$DL" ] || [ -z "$IS" ] || [ -z "$LP" ] || [ -z "$MT" ] || [ -z "$PD" ] || [ -z "$TD" ] || [ -z "$STS" ] || [ -z "$SAMPLE" ] || [ -z "$BUILD" ] || [ -z "$NO_CHR" ]; then
+            echo "Missing required parameters for VCF format"
+            usage
+        fi
+        PARAMS="--breakdancer $BD --delly $DL --insurveyor $IS --lumpy $LP --manta $MT --pindel $PD --tardis $TD --stats $STS"
+        ;;
+    *)
+        usage
+        ;;
+esac
 
-unset NXF_DISABLE_CHECK_LATEST
+LOG="${LOG_D}/.nextflow_${SAMPLE}.log"
+WORK="/tmp/scratch/${SAMPLE}"
+BASE="--containerBind $WORK --format $FMT"
+PARAMS="--build $BUILD --sample $SAMPLE --no_chr $NO_CHR ${PARAMS}"
+
+# Run nextflow command
+echo "nextflow run main.nf $BASE $PARAMS$EXTRA"
+
